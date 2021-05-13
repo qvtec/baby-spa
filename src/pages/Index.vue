81d63a23bab515eca42ebb7a595d43c52772eeb5
qvtec <qvtec3@gmail.com>
2021-05-13 10:49:06 +0900

    top layout tweak

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="logo"
-      src="~assets/logo.png"
-    >
+  <q-page>
+    <div class="flex flex-center" style="height:500px">
+      <div class="column no-wrap flex-center">
+        <img alt="logo" src="~assets/logo.png" style="width: 150px; height: 150px">
+        <div class="q-mt-md text-center">
+          <q-btn rounded color="accent" label="アカウント登録" :to="{ name: 'register' }" icon="person_add" />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
